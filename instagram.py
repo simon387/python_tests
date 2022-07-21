5f4634376105dba19d470679f678ac06dc600ffb
simone <simone.celia@eng.it>
2022-07-21 19:13:26 +0200

    ins

---
+++ instagram.py
@@ -0,0 +1,6 @@
+from instabot import Bot
+bot = Bot()
+
+if __name__ == '__main__':
+	bot.login(username="         ", password="           ")
+	bot.send_message("Hi Brother", ["Receiver's Username"])
