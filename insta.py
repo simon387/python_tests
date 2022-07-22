df2d324fd2e26ad3156e3e049c32a1b379fa5287
Simone Celia <simone.celia@simonecelia.it>
2022-07-22 17:56:57 +0200

    non_funziona

---
+++ insta.py
@@ -0,0 +1,13 @@
+# pip install instabot
+
+
+from instabot import Bot
+
+my_bot = Bot()
+# login
+
+# follow
+my_bot.follow("python_app_projects")
+
+my_bot.follow_users(["coding_for_beginners_"])
+
